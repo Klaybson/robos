86da90a459577dd81cfd25d1b3a03eb47b32b1c9
klaybson <klaybson@gmail.com>
2023-04-21 18:00:55 -0300

    atualização

--- PyAutoGui.py
+++ PyAutoGui.py
@@ -11,3 +11,7 @@ pyautogui.moveTo(x=97, y=222); pyautogui.doubleClick()
 pyautogui.moveTo(x=769, y=952); pyautogui.click()
 pyautogui.typewrite("Crie um pequeno texto sobre a biblioteca pyautogui")
 pyautogui.moveTo(x=1128, y=942); pyautogui.click()
+
+
+
+
