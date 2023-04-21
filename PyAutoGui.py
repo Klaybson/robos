87301cedea46ad19ea7520f597f4018cd8eafccd
klaybson <klaybson@gmail.com>
2023-04-21 17:51:48 -0300

    first commit

---
+++ PyAutoGui.py
@@ -0,0 +1,8 @@
+ 
+import pyautogui
+
+pyautogui.moveTo(x=81, y=119, duration=5); pyautogui.click()
+pyautogui.moveTo(x=97, y=222); pyautogui.doubleClick()
+pyautogui.moveTo(x=769, y=952); pyautogui.click()
+pyautogui.typewrite("Crie um pequeno texto sobre a biblioteca pyautogui")
+pyautogui.moveTo(x=1128, y=942); pyautogui.click()
