bb2aee442f4b10f9c5d94b39fd50b20f1244c179
klaybson <klaybson@gmail.com>
2023-04-21 19:49:03 -0300

    Modulo5

--- script.py
+++ script.py
@@ -3,11 +3,14 @@ import subprocess
 #Variavel
 comando = "ls -l /var/www/html"
 comando2 = "ls -la > ./lista_de_arquivos.txt"
-
+comando3 = "sleep 10 && echo 'Execução concluída!'"
 # executa o comando em segundo plano
-subprocess.Popen(comando, shell=True)
+subprocess.Popen(comando3, shell=True)
 
 # comando2 com saída redirecionada para um arquivo
 # executa o comando e redireciona a saída para um arquivo
 subprocess.call(comando2, shell=True)
 
+# executa o comando e captura a saída
+saida = subprocess.check_output(comando, shell=True)
+print(saida.decode('utf-8'))
