36fef5aa21c9b98138bd3ab6d9aed383e93fb62d
klaybson <klaybson@gmail.com>
2023-04-21 18:20:00 -0300

    atualização

--- PyAutoGui.py
+++ PyAutoGui.py
@@ -6,12 +6,13 @@ import pyautogui
 #Esse foi um texte para realizar uma pesquisa no chatGPt 
 #Lembre-se que é possivél passar a posição para uma variável:
 #Ex.: posicao_9 = pyautogui.position() 
-pyautogui.moveTo(x=81, y=119, duration=5); pyautogui.click()
+pyautogui.moveTo(x=81, y=119); pyautogui.click()
+pyautogui.PAUSE = 2
 pyautogui.moveTo(x=97, y=222); pyautogui.doubleClick()
 pyautogui.moveTo(x=769, y=952); pyautogui.click()
-pyautogui.typewrite("Crie um pequeno texto sobre a biblioteca pyautogui")
+pyautogui.typewrite("Você acredita que Maria Madalena engravidou por meio de um espirito?   ")
 pyautogui.moveTo(x=1128, y=942); pyautogui.click()
 
-
+pyautogui.alert('Pesquisa realizada!')
 
 
