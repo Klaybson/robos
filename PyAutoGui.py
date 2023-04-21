5332031a0ef933ca461be236592b3a393c6470e9
klaybson <klaybson@gmail.com>
2023-04-21 18:00:27 -0300

    atualização

--- PyAutoGui.py
+++ PyAutoGui.py
@@ -1,6 +1,11 @@
- 
+#Laboratorio com robo via interface
+
+#Biblioteca
 import pyautogui
 
+#Esse foi um texte para realizar uma pesquisa no chatGPt 
+#Lembre-se que é possivél passar a posição para uma variável:
+#Ex.: posicao_9 = pyautogui.position() 
 pyautogui.moveTo(x=81, y=119, duration=5); pyautogui.click()
 pyautogui.moveTo(x=97, y=222); pyautogui.doubleClick()
 pyautogui.moveTo(x=769, y=952); pyautogui.click()
